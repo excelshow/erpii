1be99b1739bf61c03b6e17917cf7733bf58fa43a
wangjunchao <378350151@qq.com>
2019-01-02 15:30:11 +0800

    wjc

--- application/controllers/billing.php
+++ application/controllers/billing.php
@@ -9,9 +9,18 @@ class Billing extends CI_Controller {
     public function index(){
         $user = $this->session->userdata('jxcsys');
 
-
+//var_dump($user);
         $this->load->view('/settings/pick_up_car');
     }
 
+    public function phone(){
+        $mobile = str_enhtml($this->input->post('userPhone',TRUE));
+        $user = $this->session->userdata('jxcsys');
 
+        $where = array(substr($user['orgWhere'],0,strrpos($user['orgWhere'],'=')) => substr($user['orgWhere'],-1,strrpos($user['orgWhere'],'=')),'mobile'=>$mobile);
+
+        $data =  $this->db->where($where)->get('ci_customer')->row();
+
+        die(json_encode($data));
+    }
 }
